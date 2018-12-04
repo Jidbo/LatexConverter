e3810168d0154db41760e413acb02bcf36e73a8a
Jidbo <gitlab@jidbo.de>
2018-12-05 00:08:55 +0100

    add option to download file directly

--- converter.py
+++ converter.py
@@ -1,11 +1,38 @@
 import pypandoc
+import os
 
 
 class Converter:
 
-    def __init__(self, md):
+    def __init__(self, md, name, to="latex", in_format="md"):
         self.md = md
+        self.to = to
+        self.in_format = in_format
+        self.name = name
+        self.filename = None
+        self.converted = None
+        self.to_convert = True
 
-    def convert_to_latex(self):
-        latex = pypandoc.convert_text(self.md, "latex", format="md", extra_args=["-s"])
-        return latex
+    def convert_to_text(self):
+        return self._convert()
+
+    def convert_to_file(self, ending):
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        tmp_directory = os.path.join(current_directory, "tmp")
+        if not os.path.isdir(tmp_directory):
+            os.makedirs(tmp_directory)
+
+        new_file = os.path.join(tmp_directory, f"{self.name}.{ending}")
+        contents = self._convert()
+        with open(new_file, "w") as file:
+            file.write(contents)
+
+        return new_file
+
+    def _convert(self):
+        if self.to_convert:
+            self.to_convert = False
+            self.converted = pypandoc.convert_text(self.md, self.to, format=self.in_format, extra_args=["-s"])
+
+        return self.converted
+
--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file, redirect
 import argparse
 from codimd import Codimd
 from converter import Converter
@@ -13,11 +13,25 @@ def home():
         codi = Codimd(url)
         codi.parse_url()
         data = codi.get_md()
-        conv = Converter(data)
-        latex = conv.convert_to_latex()
-        return render_template("index.html", data=latex, url=url)
+        conv = Converter(data, codi.note_id)
+
+        if "convertToFile" in request.form:
+            print("SAVE FILE")
+            file_path = conv.convert_to_file("md")
+            return render_template("index.html", url=url, file_path=conv.name)
+        else:
+            latex = conv.convert_to_text()
+            return render_template("index.html", data=latex, url=url)
     else:
         return render_template("index.html", url="")
+
+
+@app.route("/download/<id>", methods=["GET"])
+def downlaod(id):
+    file_path = f"tmp/{id}.md"
+    return send_file(file_path, mimetype="text/plain", as_attachment=True,
+                     attachment_filename="result.md")
+    # return redirect("/", code="302")
 
 
 if __name__ == "__main__":
@@ -26,4 +40,5 @@ if __name__ == "__main__":
                         help="Enables the debug mode.")
     args = parser.parse_args()
 
-    app.run(debug=args.debug)
+    app.run(debug=args.debug, host="0.0.0.0")
+
