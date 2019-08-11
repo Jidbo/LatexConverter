1d30e265f45b638f687d002e822ef00d33ddccfd
Jidbo <gitlab@jidbo.de>
2019-08-11 23:15:18 +0200

    fix alerts

--- app/static/src/app.js
+++ app/static/src/app.js
@@ -2,26 +2,25 @@ import './style.css';
 import './pdf.js';
 import download from 'downloadjs';
 
-$('.close').click(function() {
-	$('.alert').hide();
-});
-
-
-$('#download').click(function() {
-	download($('#result').val(), "result.tex", "text/plain");
-});
-
-$('#download-pdf').click(function() {
-	const pdfData = document.getElementById('pdf-canvas').getAttribute('data');
-	download('data:application/pdf;base64,' + pdfData, "result.pdf", "application/pdf");
-});
-
-$('#copytoclipbtn').click(function() {
-	$('#result').select();
-	document.execCommand("copy");
-	$('#copytoclip').show();
-});
-
 $(document).ready(function() {
 	$('#copytoclip').hide();
+
+	$('.close').click(function() {
+		$(this).parent().hide();
+	});
+
+	$('#download').click(function() {
+		download($('#result').val(), "result.tex", "text/plain");
+	});
+
+	$('#download-pdf').click(function() {
+		const pdfData = document.getElementById('pdf-canvas').getAttribute('data');
+		download('data:application/pdf;base64,' + pdfData, "result.pdf", "application/pdf");
+	});
+
+	$('#copytoclipbtn').click(function() {
+		$('#result').select();
+		document.execCommand("copy");
+		$('#copytoclip').show();
+	});
 });
